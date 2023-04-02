dd461be6961ea68b4b9440b3f08a86b14cfca4e4
Mun-Min <minhasmunqiz@gmail.com>
2023-04-01 17:09:21 -0700

    fixing typing/deletion animation speed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,10 +12,10 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   // const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [delta, setDelta] = useState(250); // set delta to a constant value
+  const [delta, setDelta] = useState(245); // set delta to a constant value
   const [index, setIndex] = useState(1);
   const toRotate = [ "Aspiring Data Analyst", "Software Developer", "Web Developer" ];
-  const period = 200;
+  const period = 160;
 
   useEffect(() => {
     let ticker = setInterval(() => {
