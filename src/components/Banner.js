c5fcc1207c100ac24cdd05e8a4ef2c1dcbd111e1
Mun-Min <minhasmunqiz@gmail.com>
2023-04-02 16:53:11 -0700

    fixed mobile device bug

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
@@ -7,81 +7,36 @@ import TrackVisibility from 'react-on-screen';
 import { Link } from 'react-router-dom';
 
 
-// export const Banner = () => {
-//   const [loopNum, setLoopNum] = useState(0);
-//   const [isDeleting, setIsDeleting] = useState(false);
-//   const [text, setText] = useState('');
-//   // const [delta, setDelta] = useState(300 - Math.random() * 100);
-//   const [delta, setDelta] = useState(160); // set delta to a constant value
-//   const [index, setIndex] = useState(1);
-//   const toRotate = [ " Aspiring Data Analyst", "Software Developer", "ML/AI Developer" ];
-//   const period = 900;
-
-//   useEffect(() => {
-//     let ticker = setInterval(() => {
-//       tick();
-//     }, delta);
-
-//     return () => { clearInterval(ticker) };
-//   }, [text])
-  
-
-//   const tick = () => {
-//     let i = loopNum % toRotate.length;
-//     let fullText = toRotate[i];
-//     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-//     setText(updatedText);
-
-//     if (isDeleting) {
-//       setDelta(prevDelta => prevDelta / 2);
-//     }
-
-//     if (!isDeleting && updatedText === fullText) {
-//       setIsDeleting(true);
-//       setIndex(prevIndex => prevIndex - 1);
-//       setDelta(period);
-//     } else if (isDeleting && updatedText === '') {
-//       setIsDeleting(false);
-//       setLoopNum(loopNum + 1);
-//       setIndex(1);
-//       setDelta(150);
-//     } else {
-//       setIndex(prevIndex => prevIndex + 1);
-//     }
-//   }
 export const Banner = () => {
-  const bannerRef = useRef(null);
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(160);
+  // const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(160); // set delta to a constant value
   const [index, setIndex] = useState(1);
   const toRotate = [ " Aspiring Data Analyst", "Software Developer", "ML/AI Developer" ];
   const period = 900;
-  
+
   useEffect(() => {
     let ticker = setInterval(() => {
-      if (isBannerVisible()) {
-        tick();
-      }
+      tick();
     }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
   
-    return () => clearInterval(ticker);
-  }, [text]);
-  
-  
+
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-  
+
     setText(updatedText);
-  
+
     if (isDeleting) {
       setDelta(prevDelta => prevDelta / 2);
     }
-  
+
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
       setIndex(prevIndex => prevIndex - 1);
@@ -96,15 +51,8 @@ export const Banner = () => {
     }
   }
   
-  const isBannerVisible = () => {
-    if (!bannerRef.current) return false;
-    
-    const { top, bottom } = bannerRef.current.getBoundingClientRect();
-    return top < window.innerHeight && bottom >= 0;
-  }
-  
   return (
-    <section className="banner" id="home" ref={bannerRef}>
+    <section className="banner" id="home">
       <Container style={{ minHeight: "600px", margin: "0 auto" }}>
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
