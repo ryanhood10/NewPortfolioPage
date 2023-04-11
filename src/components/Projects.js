58ab4d14ef2f2fc5cc537457c1d12d98d237e2ed
Mun-Min <minhasmunqiz@gmail.com>
2023-04-11 01:59:34 -0700

    updated Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -12,19 +15,42 @@ export const Projects = () => {
   const projects = [
     {
       title: "Project 1",
-      description: "Design & Development",
+      description: "Impacts of the Pandemic on Phoenix Housing",
       githubLink: "https://github.com/Mun-Min/Project_One",
-      // imgUrl: projImg1,
+      imgUrl: projImg1,
     },
     {
       title: "Project 2",
-      description: "Design & Development",
-      githubLink: "https://github.com/Mun-Min/Project_Two",
+      description: "Developing an Investment Portfolio via ChatBot",
+      githubLink: "https://github.com/Mun-Min/Project_3",
+      imgUrl: projImg2,
     },
     {
       title: "Project 3",
-      description: "Design & Development",
-      githubLink: "https://github.com/Mun-Min/Project_3",
+      description: "Stock Data Fetcher",
+      githubLink: "https://github.com/Mun-Min/Stock_Data_Fetcher",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const projects_tab_2 = [
+    {
+      title: "Project 4",
+      description: "Crypto Currency FOMO (Fear-of-Missing-Out) Calculator",
+      githubLink: "https://github.com/Mun-Min/Crypto_FOMO_Calculator",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Project 5",
+      description: "Forecasting Net Prophet",
+      githubLink: "https://github.com/Mun-Min/Forecasting_Net_Prophet",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Project 6",
+      description: "Machine Learning Trading Bot",
+      githubLink: "https://github.com/Mun-Min/ML_Trading_Bot",
+      imgUrl: projImg6,
     },
   ];
 
@@ -37,7 +63,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>This section exhibits expertise in Exploratory Data Analysis, Machine Learning, and Software Development with Python. It displays a collection of projects that serves as a demonstration of technical skills and experience, providing potential clients or employers an overview of the showcased work.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -45,9 +71,6 @@ export const Projects = () => {
                     </Nav.Item>
                     <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -65,12 +88,23 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <br></br>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {
+                          projects_tab_2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
+                    <br></br>
+                    <br></br>
                   </Tab.Content>
                 </Tab.Container>
               </div>}
