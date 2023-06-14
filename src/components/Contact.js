0709aa096afbb799afd07cd9b286c27fdde07762
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-06-14 15:58:06 -0500

    fixing contact form bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,7 @@ import { validateEmail } from '../utils/validators';
 
 export const Contact = () => {
   const [formData, setFormData] = useState({
+    name: '',
     email: '',
     message: ''
   });
@@ -35,6 +36,7 @@ export const Contact = () => {
     if (Object.keys(newErrors).length === 0) {
       try {
         const emailData = {
+          name: formData.name,
           email: formData.email,
           message: formData.message,
         };
@@ -62,58 +64,57 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Get In Touch</h2>
-                  <br></br>
-                  <div className="get-in-touch">
-                  <div>
-                      <p  style={{ color: 'white' }}>Name:</p>
-                      <input
-                        type="name"
-                        id="name"
-                        name="name"
-                        placeholder="Your Name"
-                        value={formData.name}
-                        onChange={handleInputChange}
-                        required
-                        className="rounded-input"
-                        style={{ color: 'white' }}
-
-                      />
-                      {errors.name && <span className="error">{errors.name}</span>}
-                    </div>
-                    <div>
-                      <p  style={{ color: 'white' }}>Email:</p>
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email Address"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        required
-                        className="rounded-input"
-                        style={{ color: 'white' }}
-
-                      />
-                      {errors.email && <span className="error">{errors.email}</span>}
-                    </div>
-                    <div>
-                      <p  style={{ color: 'white' }}>Message:</p>
-                      <textarea
-                        placeholder="Message"
-                        name="message"
-                        id="message"
-                        value={formData.message}
-                        onChange={handleInputChange}
-                        required
-                        className="rounded-textarea"
+                  <form onSubmit={handleSubmit}>
+                    <h2>Get In Touch</h2>
+                    <br></br>
+                    <div className="get-in-touch">
+                      <div>
+                        <p style={{ color: 'white' }}>Name:</p>
+                        <input
+                          type="name"
+                          id="name"
+                          name="name"
+                          placeholder="Your Name"
+                          value={formData.name}
+                          onChange={handleInputChange}
+                          required
+                          className="rounded-input"
                           style={{ color: 'white' }}
-
-                      ></textarea>
-                      {errors.message && <span className="error">{errors.message}</span>}
+                        />
+                        {errors.name && <span className="error">{errors.name}</span>}
+                      </div>
+                      <div>
+                        <p style={{ color: 'white' }}>Email:</p>
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="Email Address"
+                          value={formData.email}
+                          onChange={handleInputChange}
+                          required
+                          className="rounded-input"
+                          style={{ color: 'white' }}
+                        />
+                        {errors.email && <span className="error">{errors.email}</span>}
+                      </div>
+                      <div>
+                        <p style={{ color: 'white' }}>Message:</p>
+                        <textarea
+                          placeholder="Message"
+                          name="message"
+                          id="message"
+                          value={formData.message}
+                          onChange={handleInputChange}
+                          required
+                          className="rounded-textarea"
+                          style={{ color: 'white' }}
+                        ></textarea>
+                        {errors.message && <span className="error">{errors.message}</span>}
+                      </div>
+                      <button className="EmailButton" type="submit">Send</button>
                     </div>
-                    <button className="EmailButton" type="submit" onClick={handleSubmit}>Send</button>
-                  </div>
+                  </form>
                 </div>
               }
             </TrackVisibility>
